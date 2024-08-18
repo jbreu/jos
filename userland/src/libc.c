29c606724ae1940406cde08400dac752e1d773b2
Jakob Breu <20864561+jbreu@users.noreply.github.com>
2024-08-18 09:15:03 +0200

    removed duplication in c libc

--- userland/src/libc.c
+++ userland/src/libc.c
@@ -12,9 +12,7 @@ uint64_t strlen( const char* str ) {
     return len;
 }
 
-
-doom_boolean strcmp(const char* a, const char* b) {
-
+bool strcmp(const char* a, const char* b) {
     int i =0;
 
     while (a[i] != '\0') {
@@ -29,107 +27,39 @@ doom_boolean strcmp(const char* a, const char* b) {
     return true;
 }
 
-
-// TODO: Reduce code duplication
-
+// Write function using syscall
 void write(uint64_t filedescriptor, const char* payload, uint64_t len) {
-    register uint64_t r8 asm("r8") = filedescriptor;
-    register uintptr_t r9 asm("r9") = (uintptr_t) payload;
-    register uint64_t r10 asm("r10") = len;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 1;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"            
-        :: "r" (r8), "r" (r9), "r" (r10)
-    );
+    uint64_t result;
+    DO_SYSCALL(1, result, filedescriptor, (uintptr_t)payload, len);
 }
 
+// Get process ID
 uint64_t getpid() {
-    uint64_t _pid = 0xdeadbeef;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 2;"
-
-        "push r11;"
-        "push rcx;"
-        
-        "syscall;"
-        
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-
-        ".att_syntax;"
-        : "=a" (_pid)
-        
-    );
-
-    return _pid;
+    uint64_t pid;
+    DO_SYSCALL(2, pid, 0, 0, 0);  // No additional arguments needed
+    return pid;
 }
 
+// Draw a pixel on screen
 void draw_pixel(uint32_t x, uint32_t y, uint8_t color) {
-    register uint64_t r8 asm("r8") = x;
-    register uint64_t r9 asm("r9") = y;
-    register uint64_t r10 asm("r10") = color;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 3;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        :: "r" (r8), "r" (r9), "r" (r10)
-    );
+    uint64_t result;
+    DO_SYSCALL(3, result, x, y, color);
 }
 
-void * malloc(int size) {
-    // DBG write(1, "malloc: ", strlen("malloc: "));
-    // DBG write(1, doom_itoa(size, 10), strlen(doom_itoa(size, 10)));
-
-    uint64_t address = 0;
-    register int r8 asm("r8") = size;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 4;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        : "=a" (address)
-        : "r" (r8)
-        : "rdi", "r11", "rcx"
-    );
-
-    return (void *) address;
+// Allocate memory
+void* malloc(int size) {
+    uint64_t address;
+    DO_SYSCALL(4, address, size, 0, 0);  // Only size is passed
+    return (void*)address;
 }
 
-void free(void * address) {
-    // TODO: does nothing for now
+// Free memory (currently does nothing)
+void free(void* address) {
+    // TODO: Implement the free function
 }
 
-void * fopen(const char* filename, const char* options) {
-
-    // TODO Hack
+// Open a file
+void* fopen(const char* filename, const char* options) {
     if (!strcmp(filename, "devdatadoom1.wad")) {
         return 0;
     }
@@ -137,189 +67,67 @@ void * fopen(const char* filename, const char* options) {
     write(1, "fopen: ", strlen("fopen: "));
     write(1, filename, strlen(filename));
 
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 5;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        :
-        :
-        : "rdi", "r11", "rcx"
-    );
-
-    return (void *) 1; // TODO return non-null; dont hardcode
+    uint64_t handle;
+    DO_SYSCALL(5, handle, (uintptr_t)filename, (uintptr_t)options, 0);
+    return (void*)handle;
 }
 
+// Close a file (currently does nothing)
 void fclose(void* handle) {
-    // TODO: does nothing for now
+    // TODO: Implement the fclose function
 }
 
-
-int fwrite(void* handle, const void * foo, int bar) {
-    // TODO: does nothing for now
+// Write to a file (currently does nothing)
+int fwrite(void* handle, const void* foo, int bar) {
     write(1, "fwrite: ", strlen("fwrite: "));
+    return 0;  // TODO: Implement fwrite
 }
 
+// Seek within a file
 int fseek(void* handle, int offset, doom_seek_t origin) {
-    register uint64_t r8 asm("r8") = offset;
-    register uint64_t r9 asm("r9") = origin;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 7;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        :
-        : "r" (r8), "r" (r9)
-        : "rdi", "r11", "rcx"
-    );
+    uint64_t result;
+    DO_SYSCALL(7, result, offset, origin, 0);
+    return result;
 }
 
-
+// Check if end of file
 int feof(void* handle) {
-    uint64_t eof = 0;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 9;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        : "=a" (eof)
-        :
-        : "rdi", "r11", "rcx"
-    );
-
+    uint64_t eof;
+    DO_SYSCALL(9, eof, (uintptr_t)handle, 0, 0);
     return eof;
 }
 
+// Get the current position in a file
 int ftell(void* handle) {
-    uint64_t position = 0;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 8;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        : "=a" (position)
-        :
-        : "rdi", "r11", "rcx"
-    );
-
+    uint64_t position;
+    DO_SYSCALL(8, position, (uintptr_t)handle, 0, 0);
     return position;
 }
 
-
+// Read from a file
 int fread(void* handle, void* ptr, int size) {
-    register uintptr_t r8 asm("r8") = (uintptr_t) ptr;
-    register uintptr_t r9 asm("r9") = size;
-    int read_bytes = 0;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 6;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        : "=a" (read_bytes)
-        : "r" (r8), "r" (r9)
-        : "rdi", "r11", "rcx"
-    );
-
+    uint64_t read_bytes;
+    DO_SYSCALL(6, read_bytes, (uintptr_t)ptr, size, 0);
     return read_bytes;
 }
 
+// Draw the framebuffer
 uint64_t draw_framebuffer(const uint8_t* framebuffer) {
-    register uintptr_t r8 asm("r8") = (uintptr_t) framebuffer;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 10;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        :
-        : "r" (r8)
-        : "rdi", "r11", "rcx"
-    );
-
-    return 0;
+    uint64_t result;
+    DO_SYSCALL(10, result, (uintptr_t)framebuffer, 0, 0);
+    return result;
 }
 
+// Switch VGA mode
 uint64_t switch_vga_mode(bool vga_on) {
-    register uint64_t r8 asm("r8") = vga_on;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 11;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        :
-        : "r" (r8)
-        : "rdi", "r11", "rcx"
-    );
-
-    return 0;
+    uint64_t result;
+    DO_SYSCALL(11, result, vga_on, 0, 0);
+    return result;
 }
 
+// Get the state of a key
 bool get_keystate(int key) {
-    register uint64_t r8 asm("r8") = key;
-    int state = 0;
-
-    asm volatile (
-        ".intel_syntax noprefix;"
-        "push rdi;"
-        "mov rdi, 12;"
-        "push r11;"
-        "push rcx;"
-        "syscall;"
-        "pop rcx;"
-        "pop r11;"
-        "pop rdi;"
-        ".att_syntax;"
-        : "=a" (state)
-        : "r" (r8)
-        : "rdi", "r11", "rcx"
-    );
-
-    return (bool) state;
+    uint64_t state;
+    DO_SYSCALL(12, state, key, 0, 0);
+    return (bool)state;
 }
