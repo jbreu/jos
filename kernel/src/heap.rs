497ffeca92991834745173e36baf9770155ceb76
Jakob Breu <20864561+jbreu@users.noreply.github.com>
2024-08-18 19:51:47 +0200

    cleanup heap.rs

--- kernel/src/heap.rs
+++ kernel/src/heap.rs
@@ -7,29 +7,24 @@ use crate::mem::allocate_page_frame;
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
+const ENTRY_MASK: u64 = 0x0008_ffff_ffff_f800;
+const PAGE_ENTRY_FLAGS: u64 = 0x83; // RW + Present + Other Flags
+
 fn add_kernel_lower_l2_page_directory_table() -> u64 {
-    let entry_mask = 0x0008_ffff_ffff_f800;
     let mut kernel_cr3: u64;
 
     unsafe {
-        asm!("mov r15, cr3", out("r15") kernel_cr3);
+        // Load the CR3 register into kernel_cr3
+        asm!("mov {}, cr3", out(reg) kernel_cr3);
 
-        let l4_page_map_l4_table_start = (kernel_cr3 & entry_mask) as *const u64;
-        let l3_page_directory_pointer_table_start =
-            (*l4_page_map_l4_table_start.add(256) & entry_mask) as *const u64;
-        let l2_page_directory_table_start =
-            (*l3_page_directory_pointer_table_start & entry_mask) as *mut u64;
+        let l4_pml4_table = (kernel_cr3 & ENTRY_MASK) as *const u64;
+        let l3_pdpt = (*l4_pml4_table.add(256) & ENTRY_MASK) as *const u64;
+        let l2_page_dir = (*l3_pdpt & ENTRY_MASK) as *mut u64;
 
-        *l2_page_directory_table_start.add(20) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(21) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(22) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(23) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(24) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(25) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(26) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(27) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(28) = allocate_page_frame() | 0x83;
-        *l2_page_directory_table_start.add(29) = allocate_page_frame() | 0x83;
+        // Allocate page frames and update L2 page directory entries
+        for i in 20..30 {
+            *l2_page_dir.add(i) = allocate_page_frame() | PAGE_ENTRY_FLAGS;
+        }
     }
 
     // TODO this is hard coded, as we are adding to the 10th entry above --> make it dynamic
@@ -40,6 +35,9 @@ pub fn init_kernel_heap() {
     // TODO add more / dynamic page frames
     // TODO do not start with new page frame, but start where kernel ends
 
+    // Hardcoded heap size: 10 * 2MB pages
+    const HEAP_SIZE: usize = 0x200000 * 10;
+
     let heap_start_phys = add_kernel_lower_l2_page_directory_table();
 
     let heap_start_virtual = heap_start_phys;
@@ -47,6 +45,6 @@ pub fn init_kernel_heap() {
     unsafe {
         ALLOCATOR
             .lock()
-            .init(heap_start_virtual as *mut u8, 0x200000 * 10);
+            .init(heap_start_virtual as *mut u8, HEAP_SIZE);
     }
 }
