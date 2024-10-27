33c0e700450071bc1c3d29a973d4181f4b4d4fba
jbreu <20864561+jbreu@users.noreply.github.com>
2024-10-27 13:30:33 +0100

    deprecated semicolons

--- jbLocalTesting.ps1
+++ jbLocalTesting.ps1
@@ -9,3 +9,10 @@ Remove-Variable qemu
 
 docker run --rm -it -v "${pwd}:/root/env" jos_buildenv make -B build-x86_64
 qemu-system-x86_64 -cdrom dist/x86_64/kernel.iso
+
+
+##################
+
+docker run -it --rm --env=BOOT_MODE=legacy --env=CPU_CORES=4 --env=RAM_SIZE=4G --env=DISK_SIZE=16G --volume=/mnt/c/Users/Jakob/Documents/workspace/os-series/dist/x86_64/kernel.iso:/boot.iso -p 22 -p 5900:5900 -p 8006:8006 -p 5700:5700 --restart=no --runtime=runc --cap-add NET_ADMIN --env=DISPLAY=web --env=VGA=std --env=DEBUG=Y --device=/dev/kvm --device=/dev/dri -d qemux/qemu-docker:latest
+
+docker run --env=BOOT_MODE=legacy --env=CPU_CORES=4 --env=RAM_SIZE=4G --env=DISK_SIZE=16G  --volume=C:\Users\Jakob\Documents\workspace\os-series\dist\x86_64\kernel.iso:/boot.iso --network=bridge -p 22 -p 5900 -p 8006:8006 --restart=no --runtime=runc --cap-add NET_ADMIN --env=DISPLAY=web --device=/dev/kvm -d qemux/qemu-docker:latest
