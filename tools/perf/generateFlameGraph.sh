d84151d6537cae249f0d1ab6f6c8e3441e39937e
Jakob Breu <20864561+jbreu@users.noreply.github.com>
2025-05-10 11:50:28 +0200

    Flamegraphs 2 (#27)

--- tools/perf/generateFlameGraph.sh
+++ tools/perf/generateFlameGraph.sh
@@ -1,9 +1,12 @@
-
-
+# Remove leading '~"' from each line
 sed 's/^~"//' stacks.txt > stacks.tmp && mv stacks.tmp stacks.txt
+
+# Replace '\n' literal text with actual newlines
 sed -i 's/\\n/\n/g' stacks.txt
-#sed -i '/^Thread\|^#/!d' stacks.txt
 
 ./FlameGraph/stackcollapse-gdb.pl stacks.txt > stacks.folded
+
+# Remove stack traces that only contain unknown symbols ('??;??;')
+sed -i '/^??;??;/d' stacks.folded
 
 ./FlameGraph/flamegraph.pl stacks.folded > stacks.svg
